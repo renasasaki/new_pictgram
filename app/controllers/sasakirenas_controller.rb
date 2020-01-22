64ce6be5e5c7780a84b9619fe4671e65f93093b7
RenaSasaki <renasasaki1879@gmail.com>
2020-01-22 21:47:28 +0900

    sasakirenaコントローラーを追加

---
+++ app/controllers/sasakirenas_controller.rb
@@ -0,0 +1,2 @@
+class SasakirenasController < ApplicationController
+end
---
+++ test/controllers/sasakirenas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SasakirenasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
